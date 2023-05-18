d22de2f9617f1dcd0674618f1d931223eff5dc60
AmanWebdev2 <webdevaman2@gmail.com>
2023-05-18 14:59:24 +0530

    fixed ui alert overlapped

--- client/src/components/Alert/FormAlerter.tsx
+++ client/src/components/Alert/FormAlerter.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const FormAlerter = ({message,handleClose}:Props) => {
   return (
-    <Stack sx={{ width: '100%' }} spacing={2}>
+    <Stack sx={{ width: '100%',margin:'12px 0' }} spacing={2}>
         <Alert onClose={()=>{handleClose()}} severity="warning">{message}</Alert>
     </Stack>
   )
